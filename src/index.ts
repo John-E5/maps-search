2447a06b2071eaf6e4d5001c01f70d855a14521a
john ennis <john.ennis@livetilesglobal.com>
2024-12-03 08:44:20 +0000

    Fix issue with process undefined

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import { getPlaceAutocomplete } from './maps-api'
+import { getPlaceAutocomplete } from './maps-api';
+import { config } from 'dotenv';
+
+config();
+
 
 export async function getAutoCompleteDetails(address: any): Promise<any> {
     const apiKey = process.env.TOMTOM_API_KEY;
+    if (!apiKey) {
+        throw new Error('TOMTOM_API_KEY environment variable is not set');
+      }
     // get autocomplete results
-    const res = getPlaceAutocomplete(process.env.TOMTOM_API_KEY, address).then(async (autocompleteResults) => {
+    const res = getPlaceAutocomplete(apiKey, address).then(async (autocompleteResults) => {
         const res = []
         return res
     })
